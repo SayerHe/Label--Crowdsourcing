204ea6b9b7b5f0a20a0c561ba5dcbb1471e45ffc
Echo!!! <81091150+fyx20001011@users.noreply.github.com>
2022-03-30 17:38:50 +0800

    Update views.py

--- backEnd/labeler/views.py
+++ backEnd/labeler/views.py
@@ -8,22 +8,49 @@ from django.http import HttpResponse, JsonResponse
 
 
 def show_tasks(request):
+    global tasks
+    global num
     if request.method == 'GET':
         return render(request, "labeler/index.html")
     else:
-        tasks = LabelTasksBaseInfo.objects.all()[:]
-        dataList = [{'TaskName': i.task_name,
-                      'DataType': i.data_type,
-                      'LabelType': i.label_type,
-                      'Payment': i.task_payment,
-                      'TaskDifficulty': i.task_difficulty,
-                     'TaskDeadline':i.task_deadline.astimezone().strftime("%Y/%m/%d"),
-                     'RuleText': i.rule_file,
-                     "TaskID": i.id
-                      } for i in tasks]
+        try:
+            page = request.POST["Page"]
+            keyword = request.POST["Keyword"]
+            datatype = request.POST["Datatype"]
+            labeltype = request.POST["Labeltype"]
+            taskdifficulty = request.POST["TaskDifficulty"]
+        except:
+            return JsonResponse({'err': 'DataLost'})
 
-        print(dataList)
-        tasks_info = {"DataList":dataList}
 
-        return JsonResponse(tasks_info)
+        if page==-1:
+            tasks = LabelTasksBaseInfo.objects.all().filter(TaskName__contain=keyword) \
+                .filter(DataType__contain=datatype).filter(LabelType__contain=labeltype) \
+                .filter(TaskDifficulty__contain=taskdifficulty)
+            dataList = [{'TaskName': i.task_name,
+                         'DataType': i.data_type,
+                         'LabelType': i.label_type,
+                         'Payment': i.task_payment,
+                         'TaskDifficulty': i.task_difficulty,
+                         'TaskDeadline': i.task_deadline.astimezone().strftime("%Y/%m/%d"),
+                         'RuleText': i.rule_file,
+                         "TaskID": i.id
+                         } for i in tasks]
+            num=len(dataList)
+            new_datalist=dataList[0:10]
+            tasks_info = {"DataNumber":num,"DataList":new_datalist}
+            return JsonResponse(tasks_info)
+        else:
+            dataList = [{'TaskName': i.task_name,
+                         'DataType': i.data_type,
+                         'LabelType': i.label_type,
+                         'Payment': i.task_payment,
+                         'TaskDifficulty': i.task_difficulty,
+                         'TaskDeadline': i.task_deadline.astimezone().strftime("%Y/%m/%d"),
+                         'RuleText': i.rule_file,
+                         "TaskID": i.id
+                         } for i in tasks]
+            new_datalist=dataList[page*10:page*10+10]
+            tasks_info = {"DataNumber": num, "DataList": new_datalist}
+            return JsonResponse(tasks_info)
 
