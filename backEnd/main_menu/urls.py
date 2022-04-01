ecdc9a6837d8697197187d782b4f7567dcf9e851
SayerHe <sayer_he@163.com>
2022-04-01 17:30:27 +0800

    test push'

--- backEnd/main_menu/urls.py
+++ backEnd/main_menu/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 import sys
-from labeler import
 from main_menu import views
+from labeler.views import show_tasks
 
 app_name = "main_menu"
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
 ]
