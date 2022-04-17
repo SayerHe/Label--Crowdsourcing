2cc5fc7ce4a27e7734a0d1271566553767018f6b
Kevin <782556901@qq.com>
2022-04-17 22:18:10 +0800

    updata label

--- backEnd/labeler/static/JS/label.js
+++ backEnd/labeler/static/JS/label.js
@@ -42,8 +42,30 @@ function taskList_init(){
 
 function SubmitLabelResult(){
     label_res = document.getElementsByClassName("labelinput");
-    console.log(window.location.search);
+    taskid = window.location.search.split('&')[0].split('=')[1];
+    var labelData = [];
     for(var i = 0; i < label_res.length; i ++){
         id = label_res[i].getAttribute('id').slice(6);
+        labelData.push({
+            'id':id,
+            'label':label_res[i].value
+        })
     }
+    console.log(labelData);
+    $.ajax({
+        url: label_url,
+        type: "POST",        //请求类型
+        data: {
+            "TaskID":taskid,
+            "Labels":labelData,
+        },
+        // dataType: "json",   // 这里指定了 dateType 为json后，服务端响应的内容为json.dumps(date)，下面 success 的callback 数据无需进行JSON.parse(callback)，已经是一个对象了，如果没有指定dateType则需要执行 JSON.parse(callback)
+        success: function (callback) {
+            alert("提交成功！")
+            // userform_callback(callback['err'])
+        },
+        error: function () {
+            //当请求错误之后，自动调用
+        }
+    });
 }
