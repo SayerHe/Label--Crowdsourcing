d3bee119981d016178c9a63f09c5fb8bbf3b1f4e
SayerHe <sayer_he@163.com>
2022-06-09 21:07:17 +0800

    传ddl

--- backEnd/login/models.py
+++ backEnd/login/models.py
@@ -12,7 +12,7 @@ class UserInfo(models.Model):
     )
     user_type = models.CharField(max_length=50, choices=user_type_choice)
     salary_log = models.TextField(default=str(pd.DataFrame(columns=["TaskID", "TaskName", "ItemID", "Time", "Payment", "State"]).to_dict()))
-    task_log = models.TextField(default=str(pd.DataFrame(columns=["TaskID", "BatchID", "TaskName", "DataType", "Progress", "LastTime", "Deadline"]).to_dict()))
+    task_log = models.TextField(default=str(pd.DataFrame(columns=["TaskID", "BatchID", "TaskName", "DataType", "Progress", "LastTime", "Deadline", "TaskState"]).to_dict()))
     objects=models.Manager()
     feedback_file = models.TextField(default=str(pd.DataFrame(columns=["TaskID","TaskName","State", "ProblemDescription", "ProblemType","Priority", "ProblemDetails", "UpdatedTime"]).to_dict()))
 
