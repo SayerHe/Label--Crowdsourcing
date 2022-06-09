e481ac4e23022c690e67aec24aff934234bcce5f
Echogit config user.name <2249332707@qq.com>
2022-06-09 22:50:41 +0800

    crossnum=5

--- backEnd/labeler/views.py
+++ backEnd/labeler/views.py
@@ -16,7 +16,7 @@ ZIP_FILES = "zip_tasks"
 
 def show_tasks(request):
     DATA_ON_ONE_PAGE = 10
-    CrossNum=10
+    CrossNum=5
     if request.method == 'GET':
         if 'RequestData' not in request.GET:
             return render(request, "labeler/index.html", {'UserName':request.user.username})
